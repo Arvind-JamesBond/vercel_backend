c447b97c915e4bbdf5c30848c93f2a6c0f74368a
Arvind Kumar <arvindedla9905407353@gmail.com>
2025-11-14 19:55:52 +0530

    third

--- src/index.js
+++ src/index.js
@@ -37,7 +37,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
+app.get('/', (req, res) => {
+  res.send('Backend Project');
+});
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
